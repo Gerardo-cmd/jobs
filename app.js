da802784429a4ca2a3a6de2437c23c40c3012a1c
Gerardo Abaunza <56178845+Gerardo-cmd@users.noreply.github.com>
2021-12-09 14:02:04 -0500

    Allow localhost to CORS

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: ['localhost:3000', 'https://www.jobs.herokuapp.com'],
+    origin: ['http://localhost:3000', 'https://www.jobs.herokuapp.com'],
     allowedHeaders: ['Content-Type'],
     methods: ['GET, POST', 'DELETE'],
     optionsSuccessStatus: 200
